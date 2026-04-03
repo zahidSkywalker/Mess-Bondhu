e576d9a72896a80134bf8f6d4924c6fb82caea78
Jahidul Islam <ionzahid0987@gmail.com>
2026-04-03 22:28:10 +0600

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          db: ['dexie'],
+          pdf: ['jspdf', 'jspdf-autotable'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
